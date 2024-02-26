dd6e19d7689e1d129eb7b6ec33ad79185f0978a4
Smirnov Alex <aasmirnov@tsum.ru>
2024-02-26 09:53:42 +0300

    fix: remove ternary operator from header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -20,37 +20,47 @@ function Header({
           <div className="header__left">
             <Logo activeLogoClassName={activeLogoClassName} />
           </div>
-          {shouldRenderUser ? (
+          {shouldRenderUser && (
             <nav className="header__nav">
               <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <Link
-                    className="header__nav-link header__nav-link--profile"
-                    to={hasAccess ? AppRoutes.Favorites : AppRoutes.Login}
-                  >
-                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                    {hasAccess ? (
-                      <>
-                        <span className="header__user-name user__name">
-                          Oliver.conner@gmail.com
-                        </span>
-                        <span className="header__favorite-count">3</span>
-                      </>
-                    ) : (
+                {hasAccess && (
+                  <>
+                    <li className="header__nav-item user">
+                      <Link
+                        className="header__nav-link header__nav-link--profile"
+                        to={AppRoutes.Favorites}
+                      >
+                        <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+
+                        <>
+                          <span className="header__user-name user__name">
+                            Oliver.conner@gmail.com
+                          </span>
+                          <span className="header__favorite-count">3</span>
+                        </>
+                      </Link>
+                    </li>
+                    <li className="header__nav-item">
+                      <Link className="header__nav-link" to={AppRoutes.Main}>
+                        <span className="header__signout">Sign out</span>
+                      </Link>
+                    </li>
+                  </>
+                )}
+                {!hasAccess && (
+                  <li className="header__nav-item user">
+                    <Link
+                      className="header__nav-link header__nav-link--profile"
+                      to={AppRoutes.Login}
+                    >
+                      <div className="header__avatar-wrapper user__avatar-wrapper"></div>
                       <span className="header__login">Sign in</span>
-                    )}
-                  </Link>
-                </li>
-                {hasAccess ? (
-                  <li className="header__nav-item">
-                    <a className="header__nav-link" href="#">
-                      <span className="header__signout">Sign out</span>
-                    </a>
+                    </Link>
                   </li>
-                ) : null}
+                )}
               </ul>
             </nav>
-          ) : null}
+          )}
         </div>
       </div>
     </header>
