bde41c7306aacf5c610196daef144d87066aeb87
Smirnov Alex <aasmirnov@tsum.ru>
2024-02-26 09:54:09 +0300

    moves function get favorites to utils

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,8 +1,28 @@
-function getRating({ rating }: { rating: number }) {
+import { Offer } from '../types/offer';
+
+export const getRating = ({ rating }: { rating: number }) => {
   const STARS_COUNT = 5;
   const TOTAL_WIDTH_PERCENTAGE = 100;
 
   return (TOTAL_WIDTH_PERCENTAGE / STARS_COUNT) * rating;
-}
+};
 
-export default getRating;
+export const getFavoritesByLocation = (offers: Offer[]) =>
+  offers.reduce<{ [key: string]: Offer[] }>((acc, current) => {
+    if (!current.isFavorite) {
+      return acc;
+    }
+
+    const location = current.city.name;
+
+    if (!(location in acc)) {
+      acc[location] = [];
+    }
+    if (current.isFavorite) {
+      acc[location].push(current);
+    }
+
+    return acc;
+  }, {});
+
+export default { getRating, getFavoritesByLocation };
