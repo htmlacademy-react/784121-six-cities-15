3eac0562aab68ebd35cb1b784ca6de86758c78df
Smirnov Alex <aasmirnov@tsum.ru>
2024-03-02 08:07:04 +0300

    adds leaflet for map

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,11 +1,63 @@
 import clsx from 'clsx';
+import { useEffect, useRef } from 'react';
+import leaflet from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import useMap from './hooks';
+import { City } from '../../types/city';
+import { Offer } from '../../types/offer';
+import { URL_MARKER_CURRENT, URL_MARKER_DEFAULT } from './const';
 
 type TMapProps = {
   extraClassName?: string;
+  city: City;
+  points: Offer[];
+  selectedPoint: Offer | null;
 };
 
-function Map({ extraClassName }: TMapProps) {
-  return <section className={clsx('map', extraClassName)} />;
+function Map({ extraClassName, city, points, selectedPoint }: TMapProps) {
+  const mapRef = useRef<HTMLDivElement | null>(null);
+  const map = useMap({ city, mapRef });
+
+  const defaultCustomIcon = leaflet.icon({
+    iconUrl: URL_MARKER_DEFAULT,
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+  });
+
+  const currentCustomIcon = leaflet.icon({
+    iconUrl: URL_MARKER_CURRENT,
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+  });
+
+  useEffect(() => {
+    if (map) {
+      points.forEach((point) => {
+        leaflet
+          .marker(
+            {
+              lat: point.location.latitude,
+              lng: point.location.longitude,
+            },
+            {
+              icon:
+                point.id === selectedPoint?.id
+                  ? currentCustomIcon
+                  : defaultCustomIcon,
+            }
+          )
+          .addTo(map);
+      });
+    }
+  }, [currentCustomIcon, defaultCustomIcon, map, points, selectedPoint]);
+
+  return (
+    <section
+      style={{ height: 676 }}
+      className={clsx('map', extraClassName)}
+      ref={mapRef}
+    />
+  );
 }
 
 export default Map;
